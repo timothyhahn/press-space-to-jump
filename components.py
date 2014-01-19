dd7e7ec064792e676ce5a7c8e44c5a47ddc98b66
Timothy Hahn <timyhahn@gmail.com>
2014-01-19 13:25:49 -0500

    Created basic game

---
+++ components.py
@@ -0,0 +1,15 @@
+class Position:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+class Velocity:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+class Bounds:
+    def __init__(self, x = 5, y =  5):
+        self.x = x
+        self.y = y
+
